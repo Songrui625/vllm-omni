75a3e396cd6e18d106984c250f09d5db2cac05b1
Songrui625 <songrui625@gmail.com>
2026-04-27 20:45:52 +0800

    feat: add `prefix` parameter to match sentitive layers and add missing quant_config to add_kv_proj
    Signed-off-by: Songrui625 <songrui625@gmail.com>

--- vllm_omni/diffusion/models/flux2/flux2_transformer.py
+++ vllm_omni/diffusion/models/flux2/flux2_transformer.py
@@ -859,7 +859,7 @@ class Flux2Transformer2DModel(nn.Module):
                     mlp_ratio=mlp_ratio,
                     eps=eps,
                     bias=False,
-                    quant_config=None,
+                    quant_config=safe_quant_config(quant_config),
                     prefix=f"transformer_blocks.{i}",
                 )
                 for i in range(num_layers)
